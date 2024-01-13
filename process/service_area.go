b4e7475594b84ea58aa05d0c6b58ef1644c1702d
Sven Boor <sven.boor@gmail.com>
2024-01-13 19:55:07 +0100

    Combine data per municipality

--- process/service_area.go
+++ process/service_area.go
@@ -23,14 +23,23 @@ func (dataProcessor DataProcessor) ProcessGeofences(data []gbfs.GBFSGeofencing) 
 }
 
 func (dataProcessor DataProcessor) processGeofencesPerOperator(operatorID string, feeds []gbfs.GBFSGeofencing) {
+	var geofences []Geofence
 	for _, feed := range feeds {
-		dataProcessor.processGeofence(feed)
+		geofences = append(geofences, dataProcessor.processGeofence(feed)...)
 	}
+
+	log.Print(getGeoHashesPerMunicipality(geofences))
 }
 
-func (dataProcessor DataProcessor) processGeofence(feed gbfs.GBFSGeofencing) {
+type Geofence struct {
+	GeometryHash   string
+	Municipalities []string
+}
+
+func (dataProcessor DataProcessor) processGeofence(feed gbfs.GBFSGeofencing) []Geofence {
 	var featureCollection geojson.FeatureCollection
 	featureCollection.UnmarshalJSON(feed.Data.GeofencingZones)
+	var geofences []Geofence
 	for _, item := range featureCollection.Features {
 		res, _ := geom.SetSRID(item.Geometry, 4326)
 		obj := wkb.Geom{
@@ -38,22 +47,21 @@ func (dataProcessor DataProcessor) processGeofence(feed gbfs.GBFSGeofencing) {
 		}
 
 		q := dataProcessor.DB.QueryRow(
-			`SELECT geom_hash
+			`SELECT geom_hash, municipalities
 			FROM service_area
 			WHERE geom_hash = ENCODE(DIGEST($1::bytea, 'sha1'), 'hex')`,
 			&obj)
-		var test string
-		err := q.Scan(&test)
+		var geofence Geofence
+		err := q.Scan(&geofence.GeometryHash, pq.Array(&geofence.Municipalities))
 		if err == sql.ErrNoRows {
-			dataProcessor.insertGeofence(obj, feed.OperatorID)
+			geofence = dataProcessor.insertGeofence(obj, feed.OperatorID)
 		}
-		// if item.Properties["ride_start_allowed"].(bool) {
-		// 	result.Features = append(result.Features, item)
-		// }
+		geofences = append(geofences, geofence)
 	}
+	return geofences
 }
 
-func (dataProcessor DataProcessor) insertGeofence(geometry wkb.Geom, operatorID string) {
+func (dataProcessor DataProcessor) insertGeofence(geometry wkb.Geom, operatorID string) Geofence {
 	result, err := dataProcessor.DB.Query(
 		`SELECT municipality
 		FROM zones 
@@ -61,7 +69,7 @@ func (dataProcessor DataProcessor) insertGeofence(geometry wkb.Geom, operatorID 
 		AND ST_intersects(ST_MakeValid(st_SetSRID(ST_GeomFromWKB($1::bytea),4326)), area);`,
 		&geometry)
 	if err != nil {
-		log.Print(err)
+		return Geofence{}
 	}
 	var municipalities []string
 	for result.Next() {
@@ -70,11 +78,38 @@ func (dataProcessor DataProcessor) insertGeofence(geometry wkb.Geom, operatorID 
 		municipalities = append(municipalities, gmcode)
 	}
 
-	_, err = dataProcessor.DB.Exec(
+	res := dataProcessor.DB.QueryRow(
 		`INSERT INTO service_area (geom_hash, geom, municipalities)
 		VALUES (ENCODE(DIGEST($1::bytea, 'sha1'), 'hex'),
 			ST_MakeValid(ST_SetSRID(ST_GeomFromWKB($2::bytea), 4326)), $3)
 		returning geom_hash`,
 		&geometry, &geometry, pq.Array(municipalities))
 	log.Print(err)
+
+	var geometryHash string
+	err = res.Scan(&geometryHash)
+	if err == sql.ErrNoRows {
+		log.Printf("Something went wrong with INSERT query %+v", err)
+		return Geofence{}
+	}
+	return Geofence{
+		GeometryHash:   geometryHash,
+		Municipalities: municipalities,
+	}
+}
+
+func getGeoHashesPerMunicipality(geofences []Geofence) map[string][]string {
+	geohashPerMunicipality := map[string][]string{}
+	for _, geofence := range geofences {
+		for _, municipality := range geofence.Municipalities {
+			arrayPermunicipality, exists := geohashPerMunicipality[municipality]
+			if !exists {
+				geohashPerMunicipality[municipality] = []string{}
+				arrayPermunicipality = geohashPerMunicipality[municipality]
+			}
+			geohashPerMunicipality[municipality] = append(arrayPermunicipality, geofence.GeometryHash)
+		}
+
+	}
+	return geohashPerMunicipality
 }
